2523b4303ea464457343f4ac46ede8a6c5653039
niks87 <94444447+niks87@users.noreply.github.com>
2021-12-09 13:27:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,31 @@ Izvele = input()
 if Izvele == '1':
   pirmais()
 
+global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print("Correct Answer")
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry Wrong Answer, try again")
+            attempt = attempt + 1
+    if attempt == 3:
+        print("The Correct answer is ",answer )
+    
+score = 0
+print("atbildi uz jautajumiem")
+guess1 = input("popularaka programa kodešanai? ")
+check_guess(guess1, "python")
+guess2 = input("kura gada izveidoja python? ")
+check_guess(guess2, "1991")
+guess3 = input("vai c++ ir programma kodešanai ja vai ne? ")
+check_guess(guess3, "ja")
+guess4 = input("vai tev patik programmet? ")
+check_guess(guess4, "ja")
 
+print("Your Score is "+ str(score))
 
