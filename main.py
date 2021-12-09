01c16498a5134f1dfe3d0e12464aa9265ea3a6f5
Raivis Rasčevskis <94110450+raivisrascevskis@users.noreply.github.com>
2021-12-09 09:40:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print('Kuru no uzdevumiem vēlies realizēt')
+print('Izvēlies ciparu')
+print('1 - pirmo uzdevumu')
+print('2- otro uzdevumu')
+print('3 - trešo uzdevumu')
+print('__________________')
+print('Tu esi izvēlējies uzdevumu nr. ')
