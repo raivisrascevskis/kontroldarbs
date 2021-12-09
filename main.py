cb82b2b943c3de6b84ced0b05722ad86baa0db15
Raivis Rasčevskis <94110450+raivisrascevskis@users.noreply.github.com>
2021-12-09 09:51:46 +0000

    111

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def pirmais():
+  print('Ievadi četrus skaitļus')
+  
+
 print('Kuru no uzdevumiem vēlies realizēt')
 print('Izvēlies ciparu')
 print('1 - pirmo uzdevumu')
@@ -5,3 +9,11 @@ print('2- otro uzdevumu')
 print('3 - trešo uzdevumu')
 print('__________________')
 print('Tu esi izvēlējies uzdevumu nr. ')
+
+Izvele = input()
+
+if Izvele == '1':
+  pirmais()
+
+
+
