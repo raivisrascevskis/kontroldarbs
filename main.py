d08c1c1bb81195d507d4ee03a74bf29f591acda0
Raivis Rasčevskis <94110450+raivisrascevskis@users.noreply.github.com>
2021-12-09 13:37:03 +0200

    Update main.py
    132113

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 def pirmais():
-  print('Ievadi četrus skaitļus')
+  vertibas = []
+
+ for i in range(4):
+  a= int(input("Ievadiet vērtības"))
+  vertibas.append(a)
+
+ print(vertibas)
+
+ print(vertibas[1] * (vertibas[3]))
+  
+ def otrais():
+  print('ievadi 5 skaitļus no -10 lidz 10' )
+  count = 0
+  for i in range(5):
+  num = eval(input('Enter a number: '))
+  
+  print('your numbers are.')
+  print(num)
+
+  
   
 
-print('Kuru no uzdevumiem vēlies realizēt')
-print('Izvēlies ciparu')
-print('1 - pirmo uzdevumu')
-print('2- otro uzdevumu')
-print('3 - trešo uzdevumu')
-print('__________________')
-print('Tu esi izvēlējies uzdevumu nr. ')
-
-Izvele = input()
-
-if Izvele == '1':
-  pirmais()\
   
-  
+
+ def tresais():
   
   global score
     still_guessing = True
@@ -32,24 +40,46 @@ if Izvele == '1':
     if attempt == 3:
         print("The Correct answer is ",answer )
     
-score = 0
-print("atbildi uz jautajumiem")
-guess1 = input("popularaka programa kodešanai? ")
-check_guess(guess1, "python")
-guess2 = input("kura gada izveidoja python? ")
-check_guess(guess2, "1991")
-guess3 = input("vai c++ ir programma kodešanai ja vai ne? ")
-check_guess(guess3, "ja")
-guess4 = input("vai tev patik programmet? ")
-check_guess(guess4, "ja")
+ score = 0
+ print("atbildi uz jautajumiem")
+ guess1 = input("popularaka programa kodešanai? ")
+ check_guess(guess1, "python")
+ guess2 = input("kura gada izveidoja python? ")
+ check_guess(guess2, "1991")
+ guess3 = input("vai c++ ir programma kodešanai ja vai ne? ")
+ check_guess(guess3, "ja")
+ guess4 = input("vai tev patik programmet? ")
+ check_guess(guess4, "ja")
 
-print("Your Score is "+ str(score))
-
-print('ievadi 5 skaitļus no -10 lidz 10' )
-count = 0
-for i in range(5):
-  num = eval(input('Enter a number: '))
+ print("Your Score is "+ str(score))
   
-print('your numbers are.')
-print(num)
+  
+  
+  
+print('Kuru no uzdevumiem vēlies realizēt')
+print('Izvēlies ciparu')
+print('1 - pirmo uzdevumu')
+print('2- otro uzdevumu')
+print('3 - trešo uzdevumu')
+print('__________________')
+print('Tu esi izvēlējies uzdevumu nr. ')
+
+Izvele = input()
+
+if Izvele == '1':
+  pirmais()
+  
+if Izvelne == '2':
+  otrais()
+  
+ if izvelne == '3':
+  tresais()
+  
+  
+  1 - raivis
+  2 - eriks
+  3 - niks
+ 
+
+
 
